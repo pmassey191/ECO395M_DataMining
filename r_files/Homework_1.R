68065a8dd27b3d1dc80c9140d879a9e919894ec6
Patrick Massey <pmassey@utexas.edu>
2022-02-08 20:54:41 -0600

    Update Homework_1.R

--- r_files/Homework_1.R
+++ r_files/Homework_1.R
@@ -1,6 +1,8 @@
 library(mosaic)
 library(tidyverse)
-
+library(here)
+library(ggthemes)
+#Number 2
 billboard = read.csv('../data/billboard.csv')
 
 top10 <- billboard %>% 
@@ -38,6 +40,30 @@ ten_week_hit <- billboard %>%
 ggplot(ten_week_hit, aes(fct_rev(fct_reorder(performer,count)),count))+
   geom_bar(stat = "identity")+
   coord_flip()
-  
 
+#Number 3
+olympics_top20 = read.csv('../data/olympics_top20.csv')
 
+top_height <-olympics_top20 %>% 
+  group_by(sport) %>% 
+  summarise(
+    '95th_height' = quantile(height,probs = .95)  
+  )
+
+height_variation <- olympics_top20 %>% 
+  group_by(event) %>% 
+  summarise(
+    sd = sd(height)
+  ) %>% 
+  arrange(desc(sd))
+
+swimmers <- olympics_top20 %>% 
+  filter(sport == 'Swimming') %>% 
+  group_by(year, sex) %>% 
+  summarise(
+    avg_age = mean(age)
+  )
+ggplot(data = swimmers, aes(x = year, y = avg_age,color = sex))+
+  geom_point()+
+  geom_line()+
+  theme_fivethirtyeight()
