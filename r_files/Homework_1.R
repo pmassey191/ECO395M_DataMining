8e97063993206fdedbccb1fbe60062afb85d893b
pmassey191 <pmassey@utexas.edu>
2022-02-08 23:33:32 -0600

    Update Homework_1.R

--- r_files/Homework_1.R
+++ r_files/Homework_1.R
@@ -67,3 +67,57 @@ ggplot(data = swimmers, aes(x = year, y = avg_age,color = sex))+
   geom_point()+
   geom_line()+
   theme_fivethirtyeight()
+#Number 4
+
+sclass = read.csv('../data/sclass.csv')
+
+trim1 <- sclass %>% 
+  filter(trim == '350')
+
+trim2 <- sclass %>% 
+  filter(trim == '65 AMG')
+
+trim1_split = initial_split(trim1, prop=0.8) 
+trim1_train = training(trim1_split)
+trim1_test = testing(trim1_split)
+
+k_grid = c(2, 4, 6, 8, 10, 15, 20, 25, 30, 35, 40, 45,
+           50, 60, 70, 80, 90, 100, 125, 150, 175, 200, 250, 300)
+
+trim1_rmse = foreach(k = k_grid, .combine='rbind') %do% {
+  model = knnreg(price ~ mileage, k=k, data = trim1_train, use.all=FALSE)
+  errs = rmse(model,trim1_test)
+  c(k=k, err = mean(errs))
+} %>% as.data.frame
+
+trim1_split = initial_split(trim1, prop=0.8) 
+trim1_train = training(trim1_split)
+trim1_test = testing(trim1_split)
+
+trim2_rmse = foreach(k = k_grid, .combine='rbind') %do% {
+  model = knnreg(price ~ mileage, k=k, data = trim2_train, use.all=FALSE)
+  errs = rmse(model,trim1_test)
+  c(k=k, err = mean(errs))
+} %>% as.data.frame
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
