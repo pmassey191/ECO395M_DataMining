889e2880e3bbd42b319c27e9ee20d2b0397541f8
pmassey191 <pmassey@utexas.edu>
2022-04-30 10:53:53 -0500

    a

--- r_files/LastHW.R
+++ r_files/LastHW.R
@@ -9,12 +9,14 @@ library(igraph)
 
 groceries_raw <- read.csv("Data/groceries.csv",header = FALSE)
 
-x <- groceries_raw %>% separate(V1, 
+groceries <- groceries_raw %>% separate(V1, 
                         into =c("1","2","3","4","5","6","7","8",
-                                "9","10","11","12","13","14","15","16"),
+                                "9","10","11","12","13","14","15","16",
+                                "17","18","19","20","21","22","23","24","25",
+                                "26","27","28","29","30","31","32"),
                                 sep = ",")
 
-groceries <- groceries_raw %>% rownames_to_column(var = "customer") %>% 
+groceries <- groceries %>% rownames_to_column(var = "customer") %>% 
   mutate(customer = as.factor(customer))
 
 groceries <- pivot_longer(groceries,cols = !customer, 
@@ -24,7 +26,7 @@ groceries <- pivot_longer(groceries,cols = !customer,
 
 groceries = split(x=groceries$grocery, f=as.factor(groceries$customer))
 
-#groceries= lapply(groceries, unique)
+groceries= lapply(groceries, unique)
 
 groc_carts = as(groceries, "transactions")
 summary(groc_carts)
@@ -42,3 +44,7 @@ plot(sub1, method='graph')
 
 sub1 = subset(grocrules, subset=confidence > 0.25 & support > 0.005)
 saveAsGraph(sub1, file = "grocrules.graphml")
+inspect(sub1)
+
+write(sub1,file = "grocrules.csv")
+
